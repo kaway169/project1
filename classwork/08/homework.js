2cccbc66f7c31820c35b45c72af04f544d84f298
Yuri Kawada <kaway169@newschool.edu>
2018-11-06 13:10:21 -0500

    update

---
+++ classwork/08/homework.js
@@ -0,0 +1,11 @@
+var phrase = "" + "" + "" + array[4] + "";
+
+var one = ["a", "b", "c", "d"]
+var two = ["w", "x", "y", "z"]
+
+var yurisnumber = randomInt();
+one [yurisnumber]
+two [yurisnumber]
+
+one[randomInt()];
+two[randomInt()]
